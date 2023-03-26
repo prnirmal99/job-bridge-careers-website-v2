60e99d152e690920a4260af43a40521eb308dad5
nirmal17 <122025935+prnirmal99@users.noreply.github.com>
2023-03-26 13:44:04 +0000

    added dynamic pages for each jobs

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify, request
-from database import load_jobs_from_db
+from database import load_jobs_from_db,load_job_from_db
 from sqlalchemy import text
 
 app = Flask(__name__)
@@ -15,6 +15,11 @@ def hello_jovian():
 def list_jobs():
   jobs = load_jobs_from_db()
   return jsonify(jobs)
+
+@app.route("/job/<id>")
+def show_job(id):
+  job = load_job_from_db(id)
+  return render_template("jobpage.html",job=job)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
